3ac2eb352b4b74edde83187243dee705dffc3fe0
mr.Shu <mr@shu.io>
2014-09-02 00:24:06 +0200

    janitoring: Cleaned config.py
    * Removed rendundant (and commented out) options from config.py

--- dezider/config.py
+++ dezider/config.py
@@ -4,23 +4,9 @@ INSTALLED_PLUGINS = (
 )
 
 BOTS = [
-    # bot 1
     {
         'nick': 'dezider',
         'connections': [
-            # connect to multiple networks
-            # {
-            #     'protocol': 'xmpp',
-            #     'log_traffic': True,
-            #     'server': 'localhost', # server to connect to
-            #     'port': 5222, # default
-            #     'use_ssl': True,  # default for jabber
-            #     'keepalive_freq': '15', # default
-            #     'jabber_id':'bot@server', # depends on the server...
-            #     'password': '', # jabber_id password
-            #     'room_nick': 'bot', # nick to use in rooms, if not given, defaults to bot nick
-            #     'rooms': ['room@conference.server', ('private_room@conference.server', 'pass')]
-            # },
             {
                 'protocol': 'irc',
                 'server': 'irc.freenode.net',
@@ -41,20 +27,4 @@ BOTS = [
         },  # if this isn't set, load all
         'plugin_settings': {}
     },
-    # bot 2
-    # {
-    #     'nick': 'tester',
-    #     'connections': [
-    #         {
-    #             'protocol': 'irc',
-    #             'server': 'irc.localhost',
-    #             'port': 6667,
-    #             'use_ssl': False, # default or irc
-    #             'password': '',
-    #             'channels': ['#room', ('#private_room', 'pass')]
-    #         }
-    #     ],
-    #     'plugin_settings': {},
-    #     'command_token': '.'
-    # }
 ]
